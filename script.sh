9ad96ee7a03ce63bc999822caa22f40320581a91
khafni <khafni@student.1337.ma>
2020-03-06 14:55:51 +0100

    ok

--- script.sh
+++ script.sh
@@ -14,5 +14,6 @@ cd /tmp
 
 wget https://repo.mysql.com//mysql-apt-config_0.8.15-1_all.deb
 
-#dpkg -i *
+export DEBIAN_FRONTEND=noninteractive
 
+echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | debconf-set-selections
