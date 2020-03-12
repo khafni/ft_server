99e841719b865a28bf546370584448cd0fea713a
Khalil Hafni <khafni@e2r2p2.1337.ma>
2020-03-12 23:09:40 +0100

    ok

--- moving_script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for f in srcs/*; do docker cp $f god:/root/; done
--- srcs/script.sh
+++ srcs/script.sh
@@ -67,6 +67,5 @@ mysql --user=root --password=H wpdb < ~/wpdb.sql
 cd /var/www/html
 mv phpmyadmin/ wordpress/phpmyadmin
 mv ~/localhost* /
-bash
-#service nginx stop ; service mysql stop ; service php7.3-fpm stop
-#service nginx start ; service mysql start ; service php7.3-fpm start ; cat
+# service nginx stop ; service mysql stop ; service php7.3-fpm stop
+# service nginx start ; service mysql start ; service php7.3-fpm start
